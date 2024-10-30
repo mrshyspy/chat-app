cf142dfee527ea4771196f6735433a6a89f6fea9
sohil khan <skb90011@gmail.com>
2024-10-30 13:55:13 +0530

    SS

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:3000/", {
+			const socket = io("http://localhost:3000/https://chat-app-7b4w.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
