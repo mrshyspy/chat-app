c7ab1f9d2c2f845323bc3bd70fb54d42373c10f7
Sohil khan <92449631+mrshyspy@users.noreply.github.com>
2024-11-03 12:43:08 +0530

    Update kafka.js

--- backend/kafka.js
+++ backend/kafka.js
@@ -120,13 +120,13 @@ async function saveMessageToDatabase({ senderId, receiverId, message }) {
       message,
     });
     await newMessage.save();
-    
+
     conversation.messages.push(newMessage._id);
     await conversation.save();
 
     console.log("Message successfully saved to database:", newMessage._id);
     const messageId = `messages:${senderId}:${receiverId}`;
-    await redis.lpush(messageId, JSON.stringify(newMessage[0]));
+    await redis.lpush(messageId, JSON.stringify(newMessage));
     await redis.expire(messageId, 86400); // Expire cache after 1 day
   } catch (error) {
     console.error("Error saving message to database:", error.message || error);
